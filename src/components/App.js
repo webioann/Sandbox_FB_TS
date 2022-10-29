fff3380e9e20da53f41c0fc13d945d16a6188f67
webioann <ioannn7777@gmail.com>
2022-10-29 17:18:38 +0300

    start build new fullstack CRUD project

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react'
+import './app.scss'
+
+function App() {
+
+    return (
+        <div className='container'>
+            CRUD
+        </div>
+    )
+
+}
+export default App;
