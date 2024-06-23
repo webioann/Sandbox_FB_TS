68b7e0be8c74fb834e1cdae18dcec76e0bf87352
Webioann <ioannn7777@gmail.com>
2024-06-23 11:57:06 +0300

    trening

--- src/components_sandbox/App.tsx
+++ src/components_sandbox/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
 function App() {
-    return (
+    let text = 'Вывод скрипта будет следующим'
+
+    let isBoss = confirm('You are boss hire')
+    // alert('Hello from top')
+    
+    console.log(Boolean(isBoss));
+    // console.log(Object.isFrozen(target))
+
+        return (
         <div>App Sandbox</div>
     )
 }
