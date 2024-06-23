3624e0510e11dedcc447f07502fe70f633403f6b
Webioann <ioannn7777@gmail.com>
2024-06-23 10:26:59 +0300

    created sandbox folder

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {  store } from './app/store'
 import { Provider } from 'react-redux'
-import App from './components_redux/App'
+import App from './components_sandbox/App'
 
 ReactDOM.render( 
     <Provider store={store}>
